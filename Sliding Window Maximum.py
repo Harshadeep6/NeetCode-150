7d3ffc230d585f36362cc28186e7c2d07efbb7a0
Harshadeep <harshadeepmore@gmail.com>
2025-05-06 12:12:31 +0530

    Added

--- Sliding Window Maximum.py
+++ Sliding Window Maximum.py
@@ -1,41 +1,18 @@
-def maxSlidingWindow(nums, k):
-    
-    maxx = float("-inf")
-    maxInd = 0
-    res = []
-    l = 1
-    for ind, val in enumerate(nums):
-        if ind == k: break
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        l = r = 0
+        q = deque()
         
-        if val > maxx:
-            maxx = val
-            maxInd = ind
-        
-    res.append(maxx)
-    for r in range(k, len(nums)):
-        if l > maxInd:
-            maxx = float("-inf")
-            for j in range(l, r + 1):
-                if nums[j] > maxx:
-                    maxx = nums[j]
-                    maxInd = j
-            res.append(maxx)
-            l += 1
-            continue
+        while r < len(nums):
+            while q and nums[r] > nums[q[-1]]: q.pop()
+            q.append(r)
             
-        if nums[r] > maxx:
-            maxx = nums[r]
-            res.append(maxx)
-        else:
-            res.append(maxx)
+            if l > q[0]: q.popleft()
             
-        l += 1
-    
-    print(res)
+            if r + 1 >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
 
-# My output = [2, 2, 1, 0, 4]        
-inds = [0, 1, 2, 3, 4, 5, 6]
-nums = [1,3,-1,-3,5,3,6,7]
-k = 3
-
-maxSlidingWindow(nums, k)
+        return output
